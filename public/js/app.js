44ebec8aefc44305168561c4bdf67d07a143006b
kalyan004 <kalyansarat.04@gmail.com>
2022-01-08 15:35:54 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side Javascript file is loaded')
+
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input');
+const messageOne = document.querySelector('#sucess');
+const messageTwo = document.querySelector('#error');
+
+weatherForm.addEventListener('submit',(event) => {
+    event.preventDefault();
+    const location = searchElement.value;
+    const url = 'http://localhost:3000/weather?address='+location;
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    fetch(url).then((res) => {
+    res.json().then((data) => {
+        if(data.error) {
+            messageOne.textContent = '';
+            messageTwo.textContent = data.error;
+        }
+        else {
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
+        }
+       
+    });
+});
+});
---
+++ src/app.js
@@ -0,0 +1,97 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs');
+const geoCode = require('./utils/geoCode');
+const forecast = require('./utils/forecast');
+const { response } = require('express');
+
+
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res) => {
+    res.render('index',{
+        title : 'Weather App',
+        name : 'Kalyan'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title : 'About Me',
+        name : 'Kalyan'
+    });
+});
+
+app.get('/help' , (req,res) => {
+    res.render('help',{
+        title : 'Help',
+        name : 'Kalyan',
+        message : 'This paragraph is coming from the dynamic data . It is very nice to send this data . Also i need to test it in my browser'
+    })
+});
+
+app.get('/weather',(req,res) => {
+
+    if(!req.query.address) {
+        return res.send({
+            error : 'Address Must be Provided'
+        })
+    }
+    geoCode(req.query.address,(error,{ latitude, longitude, location } = {}) => {
+        if(error) {
+            return res.send({error})
+        }
+
+
+        forecast(latitude,longitude, (error, forecastData) => {
+                if(error){
+                    return res.send({error})
+                }
+
+            res.send(
+                {
+                    location,
+                    forecast : forecastData,
+                    address : req.query.address
+                }
+            );
+        })
+       
+    });
+   
+});
+
+
+app.get('/help/*',(req,res) => {
+    res.render('four-zero-four',{
+        title : '404',
+        name : 'Kalyan',
+        errorText : 'Help Article not found'
+    });
+});
+
+app.get('*',(req,res) => {
+    res.render('four-zero-four',{
+        title : '404',
+        name : 'Kalyan',
+        errorText : 'Page not found'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
